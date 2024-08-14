4ae0371b92e0f790ced6628ee78c040cf8623724
Ilya Dubatovka <ilya.dubatovka@mail.ru>
2024-08-14 21:18:49 +0300

    upgrade: walking animation has become smoother

--- Assets/Scripts/AnimatorController.cs
+++ Assets/Scripts/AnimatorController.cs
@@ -6,22 +6,35 @@ public class AnimatorController : MonoBehaviour
     private Animator _animator;
 
     [SerializeField] VirtualJoystick _joystick;
+
+    [SerializeField] private float _timerFreezingAnimation=1f;
+
+    private float _timer;
     // Start is called before the first frame update
     private void Awake()
     {
         _animator = GetComponent<Animator>();
+        _timer = _timerFreezingAnimation;
+        _animator.speed=0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-  
+        _timer += Time.deltaTime;
         if (_joystick.isMoving)
         {
+            _timer = 0;
             _animator.speed=1;
         }
         else
         {
+            if (_timer<=_timerFreezingAnimation)
+            {
+                _animator.speed=0.3f;
+                return;
+            }
             _animator.speed=0;
         }
     }
